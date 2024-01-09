f4c802158f881f74644a0ad23a875ee533d32754
Girish Tabaraddi <girishtabaraddi@gmail.com>
2024-01-09 09:13:51 +0100

    09-Jan-24: Initial commit for student db

---
+++ StudentDB/myCode/main.cpp
@@ -0,0 +1,16 @@
+//! Standard (system) header files
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+//! Add your project's header files here
+
+
+//! Main program
+int main ()
+{
+	cout << "StudentDB started." << endl << endl;
+
+	return 0;
+}
